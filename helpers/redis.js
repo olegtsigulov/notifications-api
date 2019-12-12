0a5db9104031542a50d8c4b3bb0ccd98f84d7571
olegtsigulov <olegcigulev@gmail.com>
2019-12-12 13:40:55 +0200

    remove forecasts logic

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,13 +1,10 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
-const redisConfig = require('../config/redis-config');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 const redisNotifyClient = redis.createClient(process.env.REDISCLOUD_URL);
-const redisForecastClient = redis.createClient(redisConfig.forecasts);
 
 module.exports = {
-  redisNotifyClient,
-  redisForecastClient,
+  redisNotifyClient
 };
